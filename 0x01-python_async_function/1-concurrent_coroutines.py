b9d92775b3cb6cc90e0e1100ede6620307f21f3f
Adam Sanusi Babatunde <iadamo.inc@gmail.com>
2024-03-23 04:38:20 +0100

    Multiple coroutines with async

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""wait_n module
+"""
+
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """wait_n: waits for a random delay between 0 and max_delay n times
+
+    args:
+        n: number of times to wait
+        max_delay: maximum delay (default 10)
+
+    returns:
+        float: the random delay
+    """
+    delays = [wait_random(max_delay) for _ in range(n)]
+    return [await delay for delay in asyncio.as_completed(delays)]
