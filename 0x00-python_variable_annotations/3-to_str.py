60b8181ca9d78d4cc735184abf0d173078d36541
Adam Sanusi Babatunde <tundey520@gmail.com>
2024-03-27 09:53:42 +0000

    Basic annotations - to string

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""to_str module
+"""
+
+
+def to_str(n: float) -> str:
+    """returns the string representation of the float.
+    """
+    return str(n)
+
+
+# pi_str = to_str(3.14)
+# print(pi_str == str(3.14))
+# print(to_str.__annotations__)
+# print("to_str(3.14) returns {} which is a {}".format(pi_str, type(pi_str)))
