b462bf0ea64faf6c5603e1aeaf7c9329ff3471e3
Adam Sanusi Babatunde <tundey520@gmail.com>
2024-03-27 08:50:39 +0000

    Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""add module
+"""
+
+
+def add(a: float, b: float) -> float:
+    """sum of two float numbers
+    """
+    return a + b
+
+
+# print(add(1.11, 2.22) == 1.11 + 2.22)
+#  print(add.__annotations__)
