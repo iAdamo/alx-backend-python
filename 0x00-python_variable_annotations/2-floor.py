f72964c491bf363de5ebb1630d1a24694ec3bdc1
Adam Sanusi Babatunde <tundey520@gmail.com>
2024-03-27 09:09:30 +0000

    Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""floor module
+"""
+
+
+def floor(n: float) -> int:
+    """returns the floor of a float number
+    """
+    return int(n)
+
+
+# import math
+#
+# ans = floor(3.14)
+#
+# print(ans == math.floor(3.14))
+# print(floor.__annotations__)
+# print("floor(3.14) returns {}, which is a {}".format(ans, type(ans)))
